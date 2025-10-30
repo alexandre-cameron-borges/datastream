ec13d351ddd37dec6424968b84d82aabe1b71cee
alexandre-cameron-borges <alexandre@ppcetoile.com>
2025-10-30 12:08:46 +0100

    Update 6A - Barème de prix par confort.sql

--- sql/6A - Barème de prix par confort.sql
+++ sql/6A - Barème de prix par confort.sql
@@ -2,4 +2,4 @@
 CREATE OR REPLACE TABLE `sorbonne-475119.projetdatastream.confort_pricing` AS
 SELECT 'Low' AS confort,    1.20 AS price_per_km UNION ALL
 SELECT 'Medium',            2.00                 UNION ALL
-SELECT 'High',              
+SELECT 'High',              3.20
